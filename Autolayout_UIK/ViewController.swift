a0fa10ff15093c505b51ec0c8c62c89b78eb671f
Israel2800 <dady281100@gmail.com>
2024-08-23 18:25:33 -0500

    Initial Commit

---
+++ Autolayout_UIK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Autolayout_UIK
+//
+//  Created by Paola Delgadillo on 8/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
